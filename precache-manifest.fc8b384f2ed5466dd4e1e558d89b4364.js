0ec35304e7d61054a587b2051ec6a0cba167db03
unknown <mszen579@hotmail.com>
2019-06-12 16:56:14 +0200

    Updates

---
+++ precache-manifest.fc8b384f2ed5466dd4e1e558d89b4364.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2df7918b7ef65cb1e652fa8ffbe47e",
+    "url": "/weatherapprj/index.html"
+  },
+  {
+    "revision": "96ca0b36f83582fbe043",
+    "url": "/weatherapprj/static/css/main.ef1cb80f.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/weatherapprj/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "96ca0b36f83582fbe043",
+    "url": "/weatherapprj/static/js/main.5456e829.chunk.js"
+  },
+  {
+    "revision": "7962aed75d72ba3ad67a",
+    "url": "/weatherapprj/static/js/runtime~main.004f8f11.js"
+  }
+]);
